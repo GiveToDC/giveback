04eed400d93250f318de68b78b9214fd575e6cf7
Quinn Lord <quinndolin404@gmail.com>
2023-08-12 16:20:12 +0100

    load all cards function and .gitignore to ignore the virtual env

---
+++ functions/load_cards/main.py
@@ -0,0 +1,43 @@
+import base64
+import functions_framework
+import json
+import requests
+import os
+
+bearer_token = os.environ.get("bearer_token")
+
+
+@functions_framework.cloud_event
+def load_cards(cloud_event):
+    data = json.loads(base64.b64decode(cloud_event.data["message"]["data"]).decode())
+    payload = {"cardIds": get_card_ids(), "amount": data["amount"]}
+    url = "https://api.dev.givecard.dev/api/v1/cards/load"
+    headers = {
+        "accept": "*/*",
+        "Authorization": bearer_token,
+        "content-type": "application/json",
+    }
+
+    try:
+        response = requests.post(url, json=payload, headers=headers)
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as error:
+        raise SystemExit(error)
+
+
+def get_card_ids():
+    url = "https://api.dev.givecard.dev/api/v1/cards"
+    headers = {
+        "accept": "*/*",
+        "Authorization": bearer_token,
+    }
+
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as error:
+        raise SystemExit(error)
+
+    cards = response.json()["cards"]
+    card_ids = list(map(lambda card: card["id"], cards))
+    return card_ids
